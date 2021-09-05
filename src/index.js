7c46b30f77a245a59e2a8f2b4cfe77a02e4b631b
SadeqSp <sadeq.sp@gmail.com>
2021-09-05 17:59:14 +0430

    5-compile & inject CSS [css-loader + style-loader]

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+require('./index.css');
+
 alert('webpack');
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,15 @@ module.exports = {
     mode: "development",
     entry: './src/index.js',
     output: {
-        path: __dirname + '/dist',
+        path: require('path').resolve(__dirname + '/dist'),
         filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
     }
 }
